d96119c445e97f77cbef720c89874871968961ae
Bill Hood <100295574+williamhood50@users.noreply.github.com>
2025-09-23 08:49:29 -0400

    # Please enter the commit message for your changes. Lines starting

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,21 +1,20 @@
 export const siteConfig = {
-  companyName: 'Titan Core',
-  siteUrl: 'https://titan-core.com',
+  companyName: 'TruckingMBA',
+  siteUrl: 'https://trucking. mba',
   Socials: {
       xSocial: 'https://x.com/astrodotbuild',
-      Github: 'https://github.com/withastro',
       Instagram: 'https://www.instagram.com/astrodotbuild',
       LinkedIn: 'https://www.linkedin.com/company/astrodotbuild',
-      Email: 'support@titan-core.com',
-      Phone: '+123 456 7890',
-      Location: '123 Main St, Anytown, USA',
+      Email: 'support@trucking.mba',
+      Phone: '+12423-641-0833',
+      Location: 'Freight Alley, Chattanooga, TN',
   }
 };
 
 export const SEO = {
   Separator: '|',
-  SiteName: 'Titan Core',
-  defaultDescription: 'A modern, high-performance Astro JS theme',
+  SiteName: 'TruckingMBA',
+  defaultDescription: 'If Knowledge is Power, then TruckingMBA is your Superpower.',
 };
   
 export const blogSetting = {
@@ -23,5 +22,5 @@ export const blogSetting = {
 }
 
 export const themeSetting = {
-  theme: 'zeus'
+  theme: 'dionysus'
 }
